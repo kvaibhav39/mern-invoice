33a94a4ab67293ad524ad39bf58c52ec649e137a
Vaibhav Khunt <kvaibhav39@gmail.com>
2024-06-08 19:34:32 +0530

    login user, get access and refresh token

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
 import registerUser from "../controllers/auth/registerController.js";
 import verifyUserEmail from "../controllers/auth/verifyEmailController.js";
-// import loginUser from "../controllers/auth/loginController.js";
-// import { loginLimiter } from "../middleware/apiLimiter.js";
+import loginUser from "../controllers/auth/loginController.js";
+import { loginLimiter } from "../middleware/apiLimiter.js";
 // import newAccessToken from "../controllers/auth/refreshTokenController.js";
 // import resendEmailVerificationToken from "../controllers/auth/resendVerifyEmailController.js";
 // import logoutUser from "../controllers/auth/logoutController.js";
@@ -21,7 +21,7 @@ router.post("/register", registerUser);
 
 router.get("/verify/:emailToken/:userId", verifyUserEmail);
 
-// router.post("/login", loginLimiter, loginUser);
+router.post("/login", loginLimiter, loginUser);
 
 // router.get("/new_access_token", newAccessToken);
 
