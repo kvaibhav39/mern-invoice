a45352b7d26266bfd1012750162859a9a72b8ef5
Vaibhav Khunt <kvaibhav39@gmail.com>
2024-06-09 08:36:27 +0530

    add resend verify, reset password request, reset password , logout api added

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,16 +4,16 @@ import verifyUserEmail from "../controllers/auth/verifyEmailController.js";
 import loginUser from "../controllers/auth/loginController.js";
 import { loginLimiter } from "../middleware/apiLimiter.js";
 import newAccessToken from "../controllers/auth/refreshTokenController.js";
-// import resendEmailVerificationToken from "../controllers/auth/resendVerifyEmailController.js";
+import resendEmailVerificationToken from "../controllers/auth/resendVerifyEmailController.js";
 // import logoutUser from "../controllers/auth/logoutController.js";
 // import passport from "passport";
 // import jwt from "jsonwebtoken";
 // import User from "../models/userModel.js";
 
-// import {
-//   resetPassword,
-//   resetPasswordRequest,
-// } from "../controllers/auth/passwordResetController.js";
+import {
+  resetPassword,
+  resetPasswordRequest,
+} from "../controllers/auth/passwordResetController.js";
 
 const router = express.Router();
 
@@ -25,11 +25,11 @@ router.post("/login", loginLimiter, loginUser);
 
 router.get("/new_access_token", newAccessToken);
 
-// router.post("/resend_email_token", resendEmailVerificationToken);
+router.post("/resend_email_token", resendEmailVerificationToken);
 
-// router.post("/reset_password_request", resetPasswordRequest);
+router.post("/reset_password_request", resetPasswordRequest);
 
-// router.post("/reset_password", resetPassword);
+router.post("/reset_password", resetPassword);
 
 // router.get("/logout", logoutUser);
 
