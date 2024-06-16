e403409ae9ed78598ffcaa1918ad9a46a6392221
Vaibhav Khunt <kvaibhav39@gmail.com>
2024-06-16 16:09:07 +0530

    add user account delete api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import getUserProfile from "../controllers/user/getUserProfile.js";
 import checkAuth from "../middleware/checkAuthMiddleware.js";
 import updateUserProfile from "../controllers/user/updateUserProfile.js";
-// import deleteMyAccount from "../controllers/user/deleteMyAccount.js";
+import deleteMyAccount from "../controllers/user/deleteMyAccount.js";
 // import getAllUserAccounts from "../controllers/user/getAllUserAccounts.js";
 // import deleteUserAccount from "../controllers/user/deleteUserAccount.js";
 // import deactivateUser from "../controllers/user/deactivateUser.js";
@@ -13,8 +13,8 @@ const router = express.Router();
 router
   .route("/profile")
   .get(checkAuth, getUserProfile)
-  .patch(checkAuth, updateUserProfile);
-//   .delete(checkAuth, deleteMyAccount);
+  .patch(checkAuth, updateUserProfile)
+  .delete(checkAuth, deleteMyAccount);
 
 // router
 //   .route("/all")
